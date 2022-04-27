9d48b259efd4a54267b14b227b169b0a2edc44a6
Dmitrii Kechin <dmitrii.kechin@gmail.com>
2022-04-27 11:14:12 +0300

    add router

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
+import './index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 import { setupStore } from './store/store';
 
@@ -9,7 +11,9 @@ const store = setupStore();
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
