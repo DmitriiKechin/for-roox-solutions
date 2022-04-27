cad4266cbb30e6be7ae76f0064b29d0f4cc10b61
Dmitrii Kechin <dmitrii.kechin@gmail.com>
2022-04-27 11:33:59 +0300

    create page 404

---
+++ src/page/notFoundPage/NotFoundPage.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './notFoudPage.module.scss';
+
+export function NotFoundPage() {
+  return <div className={styles.title}>404: Страница не найдена</div>;
+}
